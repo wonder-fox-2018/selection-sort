cdcd47cf90e65621d72ae455581d3f83f5c6a6b0
franshiro <franshiro@gmail.com>
2018-09-04 15:30:16 +0700

    ganti indexof dengan looping

--- selection_sort.js
+++ selection_sort.js
@@ -2,12 +2,15 @@ function selectionShort(values){
     let sortResult = []
    while(values.length !== 0){
        let minValue =  Math.min(...values)
-       let indexMinValue = values.indexOf(minValue)
        sortResult.push(minValue)
-       values.splice(indexMinValue, 1)
+       for(let i = 0; i < values.length; i++){
+           if(values[i] === minValue){
+               values.splice(i, 1)
+           }
+       }
    }
    return sortResult
 }
 
-console.log(selectionShort([33,2,52,106,73]))
+console.log(selectionShort([33,2,52,40,73]))
 console.log(selectionShort([13,5,22,99,11]))
