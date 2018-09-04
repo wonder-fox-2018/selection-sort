597152b6ccc0f3c4b46cf1ba2adbc5eb9b1d7651
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-09-04 17:56:23 +0700

    push selection sort

---
+++ selection_sort.js
@@ -0,0 +1,23 @@
+function selectionSort(sortIni){
+    
+    for (var i=0; i<sortIni.length-1; i++){
+        var dataSekarang=sortIni[i]
+        var nilaiMin=sortIni[i+1]
+        var indexNilaiMin=i+1
+        for (var j=i+1; j<sortIni.length; j++){
+            if (sortIni[j]<nilaiMin){
+                nilaiMin=sortIni[j]
+                indexNilaiMin=j
+            }
+        }
+        //swap data awal kalo lebih isi pada comparison
+        if (nilaiMin<dataSekarang){
+            sortIni[i]=nilaiMin
+            sortIni[indexNilaiMin]=dataSekarang
+        }
+    }
+    return sortIni
+}
+
+console.log(selectionSort([33,2,52,106,73]))
+console.log(selectionSort([13,5,22,99,11]))
